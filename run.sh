14b0a1c4c37fcdddee46500c1eede1b62997b8ad
MinhDatj <nlmdat1204@gmail.com>
2025-11-05 14:59:21 +0700

    [ADD] running program script

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 if [ ! -d "build" ]; then
     mkdir build
 fi
-gcc ./src/main.c ./src/basic_sorting.c ./src/merge_sort.c ./src/quick_sort.c ./src/test_vector.c -o ./build/main
+gcc ./src/*.c -I./include -o ./build/main
 ./build/main
